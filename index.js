74613fb74c2599c0cdf9707b6d5b83837b6b133f
david <davidcaiati@gmail.com>
2018-04-19 07:21:29 -0400

    Made a few tweeks.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 var Alexa = require("alexa-sdk");
-var AWS = require('aws-sdk');
+//var AWS = require('aws-sdk');
+var CONFIG = require('request');
 
 
 // For detailed tutorial on how to making a Alexa skill,
@@ -16,22 +17,55 @@ exports.handler = function(event, context) {
 
 var handlers = {
     'LaunchRequest': function () {
+        this.response.speak('bong');
         this.emit('AskIntent');
     },
     'AskIntent': function () {
-        console.log(this.event.request.intent);
-    
-        var s3 = new AWS.S3();
-        var params = {
-            Bucket: process.env.AUDIO_BUCKET
-        }
 
-        var index = (Math.floor(Math.random() * Math.floor(process.env.AUDIO_FILE_COUNT))) + 1; 
-        var path = "https://s3.amazonaws.com/" + process.env.AUDIO_BUCKET + "/" + index + ".mp3";
-        var audioOut = '<audio src="' + path + '" />';
-        //console.log(audioOut);
-        this.emit(':tell',audioOut);
-        
+        //console.log(this.event.request.intent);
+
+        var configFile = "https://s3.amazonaws.com/" + process.env.AUDIO_BUCKET + "/config.json";
+        var imgFile = "https://s3.amazonaws.com/" + process.env.AUDIO_BUCKET + "/yinyang.png";
+        //console.log(configFile);
+
+        var AlexaObj = this;
+        CONFIG.get(configFile, function (error, resp, body) {
+            if (!error) {
+
+                //console.log(body);
+
+                var configObj = JSON.parse(body);
+
+                //console.log(configObj);
+                
+                var noteIndex = Math.floor(Math.random() * Math.floor(process.env.AUDIO_FILE_COUNT)); 
+
+                //console.log(configObj[noteIndex]);
+
+                var cardText = configObj[noteIndex].note;
+                console.log(cardText);
+    
+                //var s3 = new AWS.S3();
+                //var params = {
+                //    Bucket: process.env.AUDIO_BUCKET
+                // }
+                var audioIndex = noteIndex + 1; 
+
+                var path = "https://s3.amazonaws.com/" + process.env.AUDIO_BUCKET + "/" + audioIndex + ".mp3";
+
+                var audioOut = "Have a better day. Contemplate this: " +
+                    '<audio src="' + path + '" />';
+
+                console.log(audioOut);
+
+                AlexaObj.emit(':tellWithCard', audioOut, "Have a Better Day", cardText, imgFile);
+
+            } else {
+                AlexaObj.emit(':tellWithCard',"Generate Kindness","Have a Better Day","Contemplate",null);
+
+            }
+ 
+        });
     },
     'SessionEndedRequest' : function() {
         console.log('Session ended with reason: ' + this.event.request.reason);
